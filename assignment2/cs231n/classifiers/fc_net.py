0e2983cbe81f6153e6a78731028538bafba81550
Nogret Humphrey <nogrethumphrey@gmail.com>
2019-03-31 22:39:08 +0200

    finish multilayer-fc

--- assignment2/cs231n/classifiers/fc_net.py
+++ assignment2/cs231n/classifiers/fc_net.py
@@ -182,21 +182,26 @@ class FullyConnectedNet(object):
         # initialized from a normal distribution centered at 0 with standard       #
         # deviation equal to weight_scale. Biases should be initialized to zero.   #
         #                                                                          #
-        # When using batch normalization, store scale and shift parameters for the #
-        # first layer in gamma1 and beta1; for the second layer use gamma2 and     #
-        # beta2, etc. Scale parameters should be initialized to ones and shift     #
-        # parameters should be initialized to zeros.                               #
+        # When using batch normalization, store scale and shift parameters for the #        self.params['W2'] = np.random.normal(0, weight_scale, [hidden_dim, num_classes])
+        # first layer in gamma1 and beta1; for the second layer use gamma2 and     #        self.params['W2'] = np.random.normal(0, weight_scale, [hidden_dim, num_classes])
+        # beta2, etc. Scale parameters should be initialized to ones and shift     #        self.params['W2'] = np.random.normal(0, weight_scale, [hidden_dim, num_classes])
+        # parameters should be initialized to zeros.                               #        self.params['W2'] = np.random.normal(0, weight_scale, [hidden_dim, num_classes])
         ############################################################################
-        length = len(hidden_dims)
+
+        ############Initialize the parameters############
+        ############Hidden layer are the layers directly after input layer and before the softmax layer###
+        length = len(hidden_dims)####number of hidden layer
         for i in range(length):
+          import pdb; pdb.set_trace()
           layer_str = str(i+1)
           if( i == 0):
             self.params['W'+layer_str] = np.random.normal(0, weight_scale, (input_dim, hidden_dims[i]))
-            self.params['b'+layer_str] = np.zeros(hidden_dim[i])
-          self.params['W'+layer_str] = np.random.normal(0, weight_scale, (hidden_dim[i-1], hidden_dims[i]))
+            self.params['b'+layer_str] = np.zeros(hidden_dims[i])
+          self.params['W'+layer_str] = np.random.normal(0, weight_scale, (hidden_dims[i-1], hidden_dims[i]))
           self.params['b'+layer_str] = np.zeros(hidden_dims[i])
 
-        self.params['W'+str(length+1)] = np.random.normal(0, weight_scale, (hidden_dims[length-1],num_classes ))
+        ##the last hidden layer
+        self.params['W'+str(length+1)] = np.random.normal(0, weight_scale, (hidden_dims[length-1],num_classes))
         self.params['b'+str(length+1)] = np.zeros(num_classes)
 
         ############################################################################
@@ -257,13 +262,23 @@ class FullyConnectedNet(object):
         # self.bn_params[1] to the forward pass for the second batch normalization #
         # layer, etc.                                                              #
         ############################################################################
+
+        #####L-1 affine_relu forward
+        #####L-1 affine_relu backward
+        #####1 affine forward
+        #####1 affine backward
+        layer_cache = [None] * (self.num_layers)
         layer_out = None
-        layer_cache = None
+
+        ##calculate all the forward for hidden layers
         for i in range(self.num_layers-1):
+          import pdb; pdb.set_trace()
           if(i == 0):
-            layer_out,layer_cache = affine_relu_forward(X,self.params['W'+str(i+1)],self.params['b'+str(i+i)])
-          layout_out,layer_cache = affine_relu_forward(layer_out,self.params['W'+str(i+1)],self.params['b'+str(i+i))
-        scores,last_layer_cache = affine_forward(layer_out,self.params['W'+str(self.num_layers)],self.params['b'+str(self.num_layers))
+            layer_out,layer_cache[i] = affine_relu_forward(X,self.params['W'+str(i+1)],self.params['b'+str(i+1)])
+          else:
+            layer_out,layer_cache[i] = affine_relu_forward(layer_out,self.params['W'+str(i+1)],self.params['b'+str(i+1)])
+        
+        scores,layer_cache[self.num_layers-1] = affine_forward(layer_out,self.params['W'+str(self.num_layers)],self.params['b'+str(self.num_layers)])
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -286,25 +301,26 @@ class FullyConnectedNet(object):
         # automated tests, make sure that your L2 regularization includes a factor #
         # of 0.5 to simplify the expression for the gradient.                      #
         ############################################################################
-        loss,loss_to_last_layer_output_gradient = softmax_loss(scores,y)
         
-        for i in reversed(range(self.num_layers)):
-          if()
-        loss += 0.5*self.reg*(np.sum(np.square(self.params['W1'])) + np.sum(np.square(self.params['W2'])))
+        ############loss should add all the regularization parameters
+        loss,loss_to_last_layer_gradient = softmax_loss(scores,y)
+        
+        ###add all the regularizations to loss
+        for i in range(self.num_layers):
+          loss += 0.5 * self.reg * np.sum(np.square(self.params['W'+str(i+1)]))
+
         if(np.sum(np.square(self.params['W1']) >= 1000)):
-          import pdb; pdb.set_trace()
+          pass
+        #  import pdb; pdb.set_trace()
 
-        loss_to_layer_one_output_gradient,dW2,db2 = affine_backward(loss_to_layer_two_output_gradient,layer_two_cache)
-
-        dW2 += self.reg * self.params['W2'] ##
-
-        grads['W2'] = dW2
-        grads['b2'] = db2
-
-        _, dW1, db1 = affine_relu_backward(loss_to_layer_one_output_gradient,layer_one_cache)
-        dW1 += self.reg * self.params['W1']
-        grads['W1'] = dW1
-        grads['b1'] = db1
+        loss_to_previous_layer_gradient = None
+        for i in reversed(self.num_layers):
+          if(i == (self.num_layers -1)):
+            loss_to_previous_layer_gradient,grads['W'+str(i+1)],grads['b'+str(i+1)] = affine_backward(loss_to_last_layer_gradient,layer_cache[i])
+          else:
+            loss_to_previous_layer_gradient,grads['W'+str(i+1)],grads['b'+str(i+1)] = affine_relu_backward(loss_to_previous_layer_gradient,layer_cache[i])
+          grads['W'+str(i+1)] += self.reg * self.params['W'+str(i+1)]
+ 
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
